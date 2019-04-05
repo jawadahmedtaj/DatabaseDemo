27bd22151d201e5b787c4279904231f897217a1f
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-04-05 22:45:14 +0200

    Complete

--- app/src/main/java/com/example/databasedemo/MainActivity.java
+++ app/src/main/java/com/example/databasedemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.databasedemo;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try{
+
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
+
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR , age INT(3))");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Jawad', 26)");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Rob', 34)");
+
+            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+
+            int nameIndex = c.getColumnIndex("name");
+            int ageIndex = c.getColumnIndex("age");
+
+            c.moveToFirst();
+            while (c != null){
+                System.out.println("Names " + c.getString(nameIndex));
+                System.out.println("Names " + Integer.toString(c.getInt(ageIndex)));
+                c.moveToNext();
+            }
+        }catch (Exception e){
+
+        }
     }
 }
