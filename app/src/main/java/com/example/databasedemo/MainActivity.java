076e68643463575782ad49599e8a6b8a686c197b
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-04-06 09:21:11 +0200

    New commands

--- app/src/main/java/com/example/databasedemo/MainActivity.java
+++ app/src/main/java/com/example/databasedemo/MainActivity.java
@@ -17,19 +17,30 @@ public class MainActivity extends AppCompatActivity {
 
             myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR , age INT(3))");
 
-            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Jawad', 26)");
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS newUsers (name VARCHAR, age INT(3), id INTEGER PRIMARY KEY)");
 
-            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Rob', 34)");
+            //myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Jawad', 26)");
 
-            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+            //myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Rob', 34)");
+
+            //myDatabase.execSQL("DELETE FROM users WHERE name = 'Rob' LIMIT 1");
+
+            //myDatabase.execSQL("UPDATE users SET age = 25 WHERE name = 'Jawad'");
+
+            //myDatabase.execSQL("DELETE FROM users WHERE name='Jawad'");
+
+
+            Cursor c = myDatabase.rawQuery("SELECT * FROM newUsers", null);
 
             int nameIndex = c.getColumnIndex("name");
             int ageIndex = c.getColumnIndex("age");
+            int idIndex = c.getColumnIndex("id");
 
             c.moveToFirst();
             while (c != null){
                 System.out.println("Names " + c.getString(nameIndex));
-                System.out.println("Names " + Integer.toString(c.getInt(ageIndex)));
+                System.out.println("Age " + Integer.toString(c.getInt(ageIndex)));
+                System.out.println("ID " + Integer.toString(c.getInt(idIndex)));
                 c.moveToNext();
             }
         }catch (Exception e){
